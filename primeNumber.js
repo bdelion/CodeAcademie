55a602ccaf46f1ffce1a51d8e14d41957a5ca39d
Bertrand DELION <bertrand.delion@free.fr>
2018-04-23 21:29:21 +0200

    Create primeNumber.js

---
+++ primeNumber.js
@@ -0,0 +1,20 @@
+/*
+Calcul les nombres premiers
+CodeAcadémie : Exercice du cours du 23/04/2018
+
+A partir d'un tableau d'entiers, écrire des codes exploitant les différents mécanismes d'itération 
+(while, for, for of, Array.forEach (cf. également Array.filter et Array.map)) pour dans un premier temps afficher 
+les nombres premiers (console.log), puis dans un second temps produire un tableau dérivé ne contenant que ces nombres premiers.
+
+Bonus : Le tableau des entiers peut-lui même être généré dynamiquement via une boucle.
+
+Indice 1 :  Un nombre premier est un nombre divisible uniquement par 1 ou par lui-même.
+            Ainsi, un nombre n est premier s'il n'est divisible par aucun des nombres premiers qui le précèdent.
+
+Indice 2 :  Un nombre a est divisible par un nombre b si le reste de la division entière de a par b est égal à 0.
+            Le reste d'une division entière s'obtient par l'opérateur % (modulo).
+*/
+
+var message = "Hello Bertrand !";
+
+console.log(message);
